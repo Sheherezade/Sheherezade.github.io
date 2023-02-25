f37c0b9847d4bd8955c8f44d66c6ec8e68b644e0
Sheherezade <675198197@qq.com>
2023-02-25 22:45:04 +0800

    Site updated: 2023-02-25 22:45:03

--- js/title.js
+++ js/title.js
@@ -1,17 +1,1 @@
-//动态标题
-var OriginTitile = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-  if (document.hidden) {
-    //离开当前页面时标签显示内容
-    document.title = '快回来🦆~';
-    clearTimeout(titleTime);
-  } else {
-    //返回当前页面时标签显示内容
-    document.title = '🐥贴贴🐥～';
-    //两秒后变回正常标题
-    titleTime = setTimeout(function () {
-      document.title = OriginTitile;
-    }, 1000);
-  }
-});
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="快回来🦆~",clearTimeout(titleTime)):(document.title="🐥贴贴🐥～",titleTime=setTimeout((function(){document.title=OriginTitile}),1e3))}));
